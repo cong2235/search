eb31e4883af7ab38850f0f966b040686dcbbc66d
cong2235 <115072482+cong2235@users.noreply.github.com>
2024-12-23 21:50:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def search_sentence():
 if __name__ == '__main__':
     # Tải TF-IDF index từ các tệp .pkl
     vectorizer, reduced_matrix = load_tfidf()
-    port = int(os.environ.get("PORT", 5000))  # Render cung cấp biến PORT
+    port = int(os.environ.get("PORT", 5001))  # Render cung cấp biến PORT
     app.run(host='0.0.0.0', port=port)
